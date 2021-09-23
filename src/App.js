cca812d685bb1d862d71f9052b5e2aa3b500622f
Rahaf <r.abualhassan@gmail.com>
2021-09-23 09:58:44 +0200

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import foodsArr from "./foods.json";
+import { v4 as uuid } from 'uuid';
+
 
 function App() {
+
+  const [foods, setFoods] = useState(foodsArr);
+
+  const foodsList = foods.map(food => {
+    return (
+      <div key={ uuid()}>
+        <p>{food.name}</p>
+        <img src={food.image} width={80} />
+      </div>
+    )
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+        IronNutrition
+        </h1>
       </header>
+      <div>
+        <h3>Food List</h3>
+        {foodsList}
+      </div>  
     </div>
   );
 }
